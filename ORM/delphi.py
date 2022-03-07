cecb16f43eac6a2de13ecce0866a7718652d994a
ebhammond <ezrahammond@gmail.com>
2022-03-06 22:15:31 -0800

    Added ORM base code

---
+++ ORM/delphi.py
@@ -0,0 +1,99 @@
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.ext.declarative import declarative_base
+
+db_username = 'postgres'
+db_password = 'postgres'
+db_hostname = 'localhost:5432'
+db_database = 'delphi'
+db_url = f'postgresql+psycopg2://{db_username}:{db_password}@{db_hostname}/{db_database}'
+
+Engine = create_engine(db_url)
+Base = declarative_base(metadata = MetaData(bind = Engine))
+
+
+class Item(Base):
+    __table__ = Table('item', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'Item({self.item_id}, {self.item_name}, {self.item_description}, ' + \
+               f'{self.item_image}, {self.price}, {self.taste_profile}, {self.item_type})'
+
+
+class Ingredient(Base):
+    __table__ = Table('ingredient', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'Ingredient({self.ingredient_id}, {self.ingredient_name})'
+
+
+class ItemIngredient(Base):
+    __table__ = Table('itemingredient', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'ItemIngredient({self.item_id}, {self.ingredient_id})'
+
+
+class NutritionFacts(Base):
+    __table__ = Table('nutritionfacts', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'NutritionFacts({self.nutrition_facts_id}, {self.item_id}, {self.serving_size}, ' + \
+               f'{self.calories}, {self.calories_from_fat}, {self.total_fat}, {self.saturated_fat}, ' + \
+               f'{self.trans_fat}, {self.cholesterol}, {self.sodium}, {self.total_carbs}, ' + \
+               f'{self.dietary_fiber}, {self.total_sugars}, {self.added_sugars}, {self.protein})'
+
+
+class Menu(Base):
+    __table__ = Table('menu', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'Menu({self.menu_id}, {self.item_id}, {self.brand}, {self.store_id})'
+
+
+class Customer(Base):
+    __table__ = Table('customer', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'Customer({self.customer_id}, {self.opt_in}, {self.birthday}, {self.gender})'
+
+
+class Order(Base):
+    __table__ = Table('order', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'Order({self.order_id}, {self.customer_id}, {self.order_timestamp}, {self.weather})'
+
+
+class OrderItem(Base):
+    __table__ = Table('orderitem', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'OrderItem({self.order_id}, {self.item_id})'
+
+
+class Customization(Base):
+    __table__ = Table('customization', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'Customization({self.customization_id}, {self.item_id}, {self.customization})'
+
+
+class OrderItemCustomization(Base):
+    __table__ = Table('orderitemcustomization', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'OrderItemCustomization({self.order_id}, {self.item_id}, {self.customization_id})'
+
+
+class DietaryPreference(Base):
+    __table__ = Table('dietarypreference', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'DietaryPreference({self.preference_id}, {self.preference}, {self.preference_weight})'
+
+
+class CustomerDietaryPreference(Base):
+    __table__ = Table('customerdietarypreference', Base.metadata, autoload = True, schema = 'delphi')
+
+    def __repr__(self):
+        return f'CustomerDietaryPreference({self.customer_id}, {self.preference_id})'
