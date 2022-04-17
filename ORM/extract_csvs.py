a6000923063149d0273aa8b8ba74a5e590ff6764
ebhammond <ezrahammond@gmail.com>
2022-04-17 00:08:47 -0700

    Updated ORM and created CSV extractor scripts

---
+++ ORM/extract_csvs.py
@@ -0,0 +1,6 @@
+import get_customer_history
+import get_item_ingredients
+
+if __name__ == '__main__':
+    get_customer_history.run()
+    get_item_ingredients.run()
--- ORM/main.py
+++ ORM/main.py
@@ -4,11 +4,18 @@ import delphi
 if __name__ == '__main__':
     session = create_session(bind = delphi.Engine)
     
+    # print('Order')
     # for row in session.query(delphi.Order).where(delphi.Order.customer_id == 5):
     #     print(row)
 
+    # print('\nNutritionFacts')
     # for row in session.query(delphi.NutritionFacts):
     #     print(row)
 
+    # print('\nItem')
     # for row in session.query(delphi.Item).where(delphi.Item.item_name == 'Cheeseburger'):
     #     print(row)
+
+    print('Item')
+    for row in session.query(delphi.Item):
+        print(row.get_item_name())
