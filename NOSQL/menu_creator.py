bf85f5f0898129ac3afd57c05b441024b2d14e38
dsoto30 <sotodaniel531@gmail.com>
2023-04-28 15:25:25 -0700

    Add orders.json and modify menu generator to add ingredients

--- NOSQL/menu_creator.py
+++ NOSQL/menu_creator.py
@@ -1,6 +1,28 @@
 import json
 import csv
 import re
+import requests
+
+"""
+Still working on grabbing ingredients using an API
+"""
+
+def get_ingredients(item):
+    app_id = 'a396278c'
+    app_key = 'ca5fb53010beec95f9f51d02515a8a47'
+    response = requests.get(f'https://api.edamam.com/api/food-database/v2/parser?app_id={app_id}&app_key={app_key}&ingr={item}')
+    if response.status_code == 200:
+        data = response.json()
+        if data['hints']:
+            for result in data['hints']:
+                if "foodContentsLabel" in result['food'] and result['food']['foodContentsLabel'] not in ["", "We are working on getting the ingredients for this item"]:
+                    if type(result['food']['foodContentsLabel']) == str:
+                        ingredients = result['food']['foodContentsLabel'].split(";")
+                        return (True, ingredients)
+                    else:
+                        return (False, [])
+    else:
+        return (False, [])
 
 def convert_mg_to_g(amount):
     return amount / 1000
@@ -19,7 +41,7 @@ def create_restaurant(restaurant_obj):
             match = re.search(r'(\d+(?:\.\d+)?)\s*(fl\s+)?(oz|g)', row["Serving Size"])
 
             if match:
-                serving_size = match.group(0)
+                #serving_size = match.group(0)
                 amount = float(match.group(1))
                 unit = match.group(3)
                 if unit == 'oz':
@@ -42,11 +64,11 @@ def create_restaurant(restaurant_obj):
                         "dietary_fiber" : float(row["Dietary Fiber"]),
                         "sugars" : float(row["Sugars"]),
                         "protein": float(row["Protein"])
-                    }
+                    },
+                    "ingredients": []
                 }
 
                 restaurant_obj["menu_items"].append(item_obj)
-
                 menu_item_id += 1
 
 
